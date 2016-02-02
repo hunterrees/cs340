bd7f532f6cd96c5054a9e5467de18a8e59d71703
Brian <tiu.brian@byu.edu>
2016-02-02 09:45:47 -0700

    Can build updates - Brian

--- Catan/test/canDoTests/CanBuildSettlement.java
+++ Catan/test/canDoTests/CanBuildSettlement.java
@@ -2,13 +2,23 @@ package canDoTests;
 
 import static org.junit.Assert.*;
 
+import map.Map;
 import org.junit.Test;
+import shared.locations.EdgeDirection;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
 
 public class CanBuildSettlement {
 
 	@Test
-	public void test() {
+	public void testMap() {
+		Map map = new Map();
+		int playerID = 1;
+		EdgeLocation el = new EdgeLocation(new HexLocation(0,0), EdgeDirection.North);
+
+
 		fail("Not yet implemented");
 	}
+
 
 }
