8bb7027c02eb35a6dbf44d940574cb45e079e0c0
Brian <tiu.brian@byu.edu>
2016-02-02 23:21:35 -0700

    Can build updates - Brian

--- Catan/test/canDoTests/CanBuildSettlement.java
+++ Catan/test/canDoTests/CanBuildSettlement.java
@@ -1,11 +1,13 @@
 package canDoTests;
 
 import map.Map;
+import map.Vertex;
 import org.junit.Test;
-import shared.locations.HexLocation;
-import shared.locations.VertexDirection;
-import shared.locations.VertexLocation;
+import shared.Piece;
+import shared.definitions.PieceType;
+import shared.locations.*;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -15,26 +17,53 @@ public class CanBuildSettlement {
     public void testMap() {
         assertTrue(true);
 
-        // Testing building a settlement on an open map
+        // Testing building a settlement on an open map on setup mode
         Map map = new Map();
         int playerID = 1;
         VertexLocation el = new VertexLocation(new HexLocation(0, 0), VertexDirection.East).getNormalizedLocation();
-        boolean possible = map.canBuildSettlement(playerID, true, el);
+        assertTrue(map.canBuildSettlement(playerID, true, el));
 
 
-        if (possible) {
-            assertTrue(true);
-        } else {
-            fail("Doesn't work");
-        }
+        // Building a settlement on an open map not on setup mode
+        assertFalse(map.canBuildSettlement(playerID, false, el));
 
         // Building it next to another settlement
+        Vertex spot = map.getVerticies().get(new VertexLocation(new HexLocation(0,0), VertexDirection.East).getNormalizedLocation());
+        Piece house = new Piece(PieceType.SETTLEMENT,null, null, 1);
+
+        spot.setPiece(house);
+        el = new VertexLocation(new HexLocation(0, 0), VertexDirection.NorthEast).getNormalizedLocation();
+        assertFalse(map.canBuildSettlement(1, true, el));
 
         // Building it on another settlement
+        el = new VertexLocation(new HexLocation(0, 0), VertexDirection.East).getNormalizedLocation();
+        assertFalse(map.canBuildSettlement(1, true, el));
 
-        // Building it next to another road
+        // Building it next to own road on non setup mode
+        // This test case is failing
+        Piece road = new Piece(PieceType.ROAD, null, null, 1);
+        spot = map.getVerticies().get(new EdgeLocation(new HexLocation(0,0), EdgeDirection.South).getNormalizedLocation());
+        spot.setPiece(road);
+        el = new VertexLocation(new HexLocation(0, 0), VertexDirection.SouthWest).getNormalizedLocation();
+        assertTrue(map.canBuildSettlement(1, true, el));
+
+        // Building it next to another player's road on non setup mode
+        // This test case is failing
+        road = new Piece(PieceType.ROAD, null, null, 2);
+        spot = map.getVerticies().get(new EdgeLocation(new HexLocation(0,0), EdgeDirection.South).getNormalizedLocation());
+        spot.setPiece(road);
+        el = new VertexLocation(new HexLocation(0, 0), VertexDirection.SouthWest).getNormalizedLocation();
+        assertTrue(map.canBuildSettlement(1, true, el));
+
+
+
+
+
+
+
 
         // Building it at the side of the board
+
 
     }
 
