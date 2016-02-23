a3cc73fd4af109bc2d3079217e8e96c0910fe07f
ztnelson01 <zach.t.nelson01@gmail.com>
2016-02-23 10:11:39 -0700

    Pushing (hopefully completed) turnTracker

--- Catan/src/client/turntracker/TurnTrackerController.java
+++ Catan/src/client/turntracker/TurnTrackerController.java
@@ -1,10 +1,14 @@
 package client.turntracker;
 
 import shared.definitions.CatanColor;
+import shared.definitions.GameState;
 
 import java.util.Observable;
 
 import client.base.*;
+import gameManager.GameManager;
+import model.GameException;
+import player.Player;
 
 
 /**
@@ -12,6 +16,12 @@ import client.base.*;
  */
 public class TurnTrackerController extends Controller implements ITurnTrackerController {
 
+	private GameState myGameState;
+	private int currentPlayerIndex;
+	GameManager myManager;
+	boolean initiated = false;
+	boolean endTurnEnabled = false;
+	
 	public TurnTrackerController(ITurnTrackerView view) {
 		
 		super(view);
@@ -25,22 +35,98 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 		return (ITurnTrackerView)super.getView();
 	}
 
+	
 	@Override
 	public void endTurn() {
-
+		if (endTurnEnabled)
+		{
+			try {
+				myManager.finishTurn(myManager.getPlayerInfo().getId());
+			} catch (GameException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	private void initFromModel() {
 		//<temp>
 		getView().setLocalPlayerColor(CatanColor.RED);
+		/*
 		//</temp>
+		 * 
+		 */
+	}
+	
+	private void initiate()
+	{
+		getView().setLocalPlayerColor(myManager.getPlayerInfo().getColor());
+		for (int i = 0; i < 4; i++)
+		{
+			String playerName = myManager.getModel().getName(i);
+			CatanColor playerColor = myManager.getColor(i);
+			getView().initializePlayer(i, playerName, playerColor);
+		}
+	}
+	
+	private void updatePlayer()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			Player tempPlayer = myManager.getModel().getPlayers().get(i);
+			int playerIndex = i;
+			int points = tempPlayer.getVictoryPoints();
+			boolean highlight;
+			if (i == myManager.getCurrentPlayerIndex())
+			{
+				highlight = true;
+			}
+			else 
+			{
+				highlight = false;
+			}
+			boolean largestArmy = false;
+			boolean longestRoad = false;
+			getView().updatePlayer(playerIndex, points, highlight, largestArmy, longestRoad);
+		}
+	}
+	
+	private void updateGameState()
+	{
+		endTurnEnabled = false;
+		String buttonMessage = "";
+		
+		if (myGameState == GameState.rolling || myGameState == GameState.robbing || myGameState == GameState.discarding
+				|| currentPlayerIndex != myManager.getPlayerInfo().getPlayerIndex())
+		{
+			buttonMessage = "Waiting for Other Players";
+			endTurnEnabled = false;
+		}
+		else
+		{
+			buttonMessage = "Finish Turn";
+			endTurnEnabled = true;
+		}
+		getView().updateGameState(buttonMessage, endTurnEnabled);
 	}
 	
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
 		
+		// TODO Auto-generated method stub
+		if (!initiated)
+		{
+			initiate();
+			initiated = true;
+		}
+		myManager = (GameManager)o;
+		currentPlayerIndex = myManager.getCurrentPlayerIndex();
+		myGameState = myManager.getGameState();
+		
+		updateGameState();
+		updatePlayer();
 	}
-
 }
+
+
 
