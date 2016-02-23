db46a3294e01ce125569ccf75115cb3cbc868cf7
ztnelson01 <zach.t.nelson01@gmail.com>
2016-02-23 11:22:18 -0700

    Added functionality such that the number of resources should be displayed and action buttons should be enabled/disabled according to whether enough resources are available.

--- Catan/src/client/resources/ResourceBarController.java
+++ Catan/src/client/resources/ResourceBarController.java
@@ -3,6 +3,12 @@ package client.resources;
 import java.util.*;
 
 import client.base.*;
+import client.data.PlayerInfo;
+import gameManager.GameManager;
+import model.GameException;
+import player.Player;
+import shared.definitions.PieceType;
+import shared.definitions.ResourceType;
 
 
 /**
@@ -69,10 +75,92 @@ public class ResourceBarController extends Controller implements IResourceBarCon
 		}
 	}
 
+	private void setResourceAmounts(Player myPlayer)
+	{
+		getView().setElementAmount(ResourceBarElement.BRICK, myPlayer.getPlayerHand().numResourceOfType(ResourceType.BRICK));
+		getView().setElementAmount(ResourceBarElement.WOOD, myPlayer.getPlayerHand().numResourceOfType(ResourceType.WOOD));
+		getView().setElementAmount(ResourceBarElement.ORE, myPlayer.getPlayerHand().numResourceOfType(ResourceType.ORE));
+		getView().setElementAmount(ResourceBarElement.WHEAT, myPlayer.getPlayerHand().numResourceOfType(ResourceType.WHEAT));
+		getView().setElementAmount(ResourceBarElement.SHEEP, myPlayer.getPlayerHand().numResourceOfType(ResourceType.SHEEP));
+		getView().setElementAmount(ResourceBarElement.ROAD, myPlayer.numPiecesOfType(PieceType.ROAD));
+		getView().setElementAmount(ResourceBarElement.SETTLEMENT, myPlayer.numPiecesOfType(PieceType.SETTLEMENT));
+		getView().setElementAmount(ResourceBarElement.CITY, myPlayer.numPiecesOfType(PieceType.CITY));
+		getView().setElementAmount(ResourceBarElement.SOLDIERS, myPlayer.getSoldiers());
+	}
+	private void enableDisableButtons(GameManager myManager, Player myPlayer, PlayerInfo myPlayerInfo)
+	{
+		boolean myTurn;
+		if (myManager.getCurrentPlayerIndex() == myPlayerInfo.getId())
+		{
+			myTurn = true;
+		}
+		else
+		{
+			myTurn = false;
+		}
+		if (myTurn)
+		{
+			//if my turn I can bring up dev card thing
+			getView().setElementEnabled(ResourceBarElement.PLAY_CARD, true);
+			//check resources for buying dev card
+			if (myManager.getModel().buyDevCard(myPlayerInfo.getId()))
+			{
+				getView().setElementEnabled(ResourceBarElement.BUY_CARD, true);
+			}
+			else
+			{
+				getView().setElementEnabled(ResourceBarElement.BUY_CARD, false);
+			}
+			//check resources for buying Road
+			if (myPlayer.canBuildRoad())
+			{
+				getView().setElementEnabled(ResourceBarElement.ROAD, true);
+			}
+			else 
+			{
+				getView().setElementEnabled(ResourceBarElement.ROAD, false);
+			}
+			//check resources for buying settlement
+			if (myPlayer.canBuildSettlement())
+			{
+				getView().setElementEnabled(ResourceBarElement.SETTLEMENT, true);
+			}
+			else 
+			{
+				getView().setElementEnabled(ResourceBarElement.SETTLEMENT, false);
+			}
+			//check resources for buying city
+			if (myPlayer.canBuildCity())
+			{
+				getView().setElementEnabled(ResourceBarElement.CITY, true);
+			}
+			else
+			{
+				getView().setElementEnabled(ResourceBarElement.CITY, false);
+			}
+		}
+		else
+		{
+			getView().setElementEnabled(ResourceBarElement.BUY_CARD, false);
+			getView().setElementEnabled(ResourceBarElement.SETTLEMENT, false);
+			getView().setElementEnabled(ResourceBarElement.CITY, false);
+			getView().setElementEnabled(ResourceBarElement.ROAD, false);
+			getView().setElementEnabled(ResourceBarElement.PLAY_CARD, false);
+		}
+	}
+	
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+		//Get manager
+		GameManager myManager = (GameManager)o;
+		//Get player
+		PlayerInfo myPlayerInfo = myManager.getPlayerInfo();
+		Player myPlayer = myManager.getModel().getPlayers().get(myPlayerInfo.getPlayerIndex());
+		//Set element amount (resources)
+		setResourceAmounts(myPlayer);
+		//Set element enabled/disabled(action buttons)
+		enableDisableButtons(myManager, myPlayer, myPlayerInfo);
 	}
 }
 
