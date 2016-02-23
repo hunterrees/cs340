fc6b9246778d2b3e14db9e41b8616d4f12bcaf75
Brian <tiu.brian@byu.edu>
2016-02-22 18:10:10 -0700

    map updates - brian

--- Catan/src/client/states/NotYourTurn.java
+++ Catan/src/client/states/NotYourTurn.java
@@ -1,7 +1,12 @@
 package client.states;
 
+import map.Map;
+
 /**
  * Created by Brian on 16/2/17.
  */
 public class NotYourTurn extends State {
+    public NotYourTurn(Map map) {
+        super(map);
+    }
 }
--- Catan/src/client/states/Robbing.java
+++ Catan/src/client/states/Robbing.java
@@ -1,7 +1,12 @@
 package client.states;
 
+import map.Map;
+
 /**
  * Created by Brian on 16/2/17.
  */
 public class Robbing extends State {
+    public Robbing(Map map) {
+        super(map);
+    }
 }
