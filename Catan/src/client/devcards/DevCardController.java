ead59b8c89da1118e694f190ae20ea1b9ba4ccfe
hunterrees <hunterrees13@gmail.com>
2016-02-24 16:34:57 -0700

    Dev Card Controller

--- Catan/src/client/devcards/DevCardController.java
+++ Catan/src/client/devcards/DevCardController.java
@@ -1,10 +1,15 @@
 package client.devcards;
 
+import shared.DevelopmentCard;
+import shared.definitions.DevCardType;
 import shared.definitions.ResourceType;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 import client.base.*;
+import gameManager.GameManager;
+import player.Player;
 
 
 /**
@@ -15,6 +20,7 @@ public class DevCardController extends Controller implements IDevCardController 
 	private IBuyDevCardView buyCardView;
 	private IAction soldierAction;
 	private IAction roadAction;
+	private Player player;
 	
 	/**
 	 * DevCardController constructor
@@ -44,59 +50,116 @@ public class DevCardController extends Controller implements IDevCardController 
 
 	@Override
 	public void startBuyCard() {
-		
 		getBuyCardView().showModal();
 	}
 
 	@Override
 	public void cancelBuyCard() {
-		
 		getBuyCardView().closeModal();
 	}
 
 	@Override
 	public void buyCard() {
-		
-		getBuyCardView().closeModal();
+		try{
+			GameManager.getInstance().buyDevCard(GameManager.getInstance().getPlayerInfo().getPlayerIndex());
+			getBuyCardView().closeModal();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void startPlayCard() {
-		
+		int index = GameManager.getInstance().getPlayerInfo().getPlayerIndex();
+		player = GameManager.getInstance().getModel().getPlayers().get(index);
+		ArrayList<DevelopmentCard> oldCards = player.getPlayerHand().getOldDevelopmentCards();
+		ArrayList<DevelopmentCard> newCards = player.getPlayerHand().getNewDevelopmentCards();
+		int monopoly = 0;
+		int soldier = 0;
+		int monument = 0;
+		int yearOfPlenty = 0;
+		int roadBuild = 0;
+		for(int i = 0; i < oldCards.size(); i++){
+			switch(oldCards.get(i).getType()){
+				case MONOPOLY: monopoly++; break;
+				case SOLDIER: soldier++; break;
+				case MONUMENT: monument++; break;
+				case YEAR_OF_PLENTY: yearOfPlenty++; break;
+				case ROAD_BUILD: roadBuild++; break;
+			}
+		}
+		for(int i = 0; i < newCards.size(); i++){
+			switch(newCards.get(i).getType()){
+				case MONOPOLY: monopoly++; break;
+				case SOLDIER: soldier++; break;
+				case MONUMENT: monument++; break;
+				case YEAR_OF_PLENTY: yearOfPlenty++; break;
+				case ROAD_BUILD: roadBuild++; break;
+			}
+		}
+		getPlayCardView().setCardAmount(DevCardType.MONOPOLY, monopoly);
+		getPlayCardView().setCardAmount(DevCardType.SOLDIER, soldier);
+		getPlayCardView().setCardAmount(DevCardType.MONUMENT, monument);
+		getPlayCardView().setCardAmount(DevCardType.YEAR_OF_PLENTY, yearOfPlenty);
+		getPlayCardView().setCardAmount(DevCardType.ROAD_BUILD, roadBuild);
+		getPlayCardView().setCardEnabled(DevCardType.MONOPOLY, false);
+		getPlayCardView().setCardEnabled(DevCardType.SOLDIER, false);
+		getPlayCardView().setCardEnabled(DevCardType.MONUMENT, false);
+		getPlayCardView().setCardEnabled(DevCardType.YEAR_OF_PLENTY, false);
+		getPlayCardView().setCardEnabled(DevCardType.ROAD_BUILD, false);
+		if(!player.isHasPlayedDevCard()){
+			for(int i = 0; i < oldCards.size(); i++){
+				getPlayCardView().setCardEnabled(oldCards.get(i).getType(), true);
+			}
+			for(int i = 0; i < newCards.size(); i++){
+				if(newCards.get(i).getType() == DevCardType.MONOPOLY){
+					getPlayCardView().setCardEnabled(DevCardType.MONOPOLY, true);
+				}
+			}
+		}
 		getPlayCardView().showModal();
 	}
 
 	@Override
 	public void cancelPlayCard() {
-
 		getPlayCardView().closeModal();
 	}
 
 	@Override
 	public void playMonopolyCard(ResourceType resource) {
-		
+		try{
+			GameManager.getInstance().monopoly(GameManager.getInstance().getPlayerInfo().getPlayerIndex(), resource);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void playMonumentCard() {
-		
+		try{
+			GameManager.getInstance().monument(GameManager.getInstance().getPlayerInfo().getPlayerIndex());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void playRoadBuildCard() {
-		
 		roadAction.execute();
 	}
 
 	@Override
 	public void playSoldierCard() {
-		
 		soldierAction.execute();
 	}
 
 	@Override
 	public void playYearOfPlentyCard(ResourceType resource1, ResourceType resource2) {
-		
+		try{
+			GameManager.getInstance().yearOfPlenty(GameManager.getInstance().getPlayerInfo().getPlayerIndex(), resource1, resource2);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
