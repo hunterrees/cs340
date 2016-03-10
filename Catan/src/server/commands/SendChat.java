c9cf518942f4c44c6701ae1d6373de8c0e3d4d40
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 13:55:12 -0700

    sendChat javadoc

--- Catan/src/server/commands/SendChat.java
+++ Catan/src/server/commands/SendChat.java
@@ -9,6 +9,10 @@ public class SendChat extends Command{
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Preconditions: None. Message may be sent at any time by any player.
+	 * PostConditions: The message is sent to the chat log.
+	 */
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
