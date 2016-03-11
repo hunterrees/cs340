3bed133742f32a3daa912e37d517155815025b2a
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 21:54:41 -0700

    Added Javadoc to RollNumber

--- Catan/src/server/commands/moves/RollNumber.java
+++ Catan/src/server/commands/moves/RollNumber.java
@@ -10,6 +10,11 @@ public class RollNumber extends Command{
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Preconditions: It's the players turn
+	 * 					The status of the player's model is 'rolling'
+	 * Postconditions: Changes the status of the player to 'Discarding, Robbing, or Playing'
+	 */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
