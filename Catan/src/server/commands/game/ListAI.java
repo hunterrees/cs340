1ed8ea17d71c37a89d42b243fca55b70a03dcae0
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 21:51:59 -0700

    Added ListAI with Javadoc

---
+++ Catan/src/server/commands/game/ListAI.java
@@ -0,0 +1,23 @@
+package server.commands.game;
+
+import server.commands.Command;
+import shared.model.GameModel;
+
+public class ListAI extends Command {
+
+	public ListAI(GameModel model, String json) {
+		super(model, json);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * Preconditions: none
+	 * PostConditions: Returns HTTP 200 response with success in body
+	 * 					Returns JSON array of the types of AI players that can be added to a game
+	 */
+	@Override
+	public Object execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
