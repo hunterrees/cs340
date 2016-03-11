8d95b8ff308cb3b6b1e853272594ac4574bca419
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 22:00:53 -0700

    Year of plenty added Javadocs

--- Catan/src/server/commands/moves/YearOfPlenty.java
+++ Catan/src/server/commands/moves/YearOfPlenty.java
@@ -10,6 +10,11 @@ public class YearOfPlenty extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Preconditions: The two specified resources are in the bank
+	 * Postconditions: The two specified resources are removed from the bank and added to the hand of the 
+	 * 					player.
+	 */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
