c070c4d4b67208214a87321f044200dd58444a31
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 22:05:50 -0700

    Added Road Building Javadoc

--- Catan/src/server/commands/moves/RoadBuilding.java
+++ Catan/src/server/commands/moves/RoadBuilding.java
@@ -10,6 +10,17 @@ public class RoadBuilding extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Preconditions: The first road location (spot1) is connected to one of your roads.
+		*				The second road location (spot2) is connected to one of your roads or to the first
+		*				road location (spot1)
+		*				Neither road location is on water
+		*				You have at least two unused roads
+	 * Postconditions: Usable roads in hand of player is reduced by 2.
+	 * 					The roads are built in the specified locations 
+	 * 					If applicable longest road is given to the player.
+
+	 */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
