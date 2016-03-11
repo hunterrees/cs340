aeb460076a0ed4c06a7311757d2a8af936cc6175
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 22:45:55 -0700

    Added Javadoc to Soldier

--- Catan/src/server/commands/moves/Soldier.java
+++ Catan/src/server/commands/moves/Soldier.java
@@ -10,6 +10,14 @@ public class Soldier extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Preconditions: The robber is is being moved from its current location
+	 * 					If a player is being robbed, that player has resource cards
+	 * Postconditions: The robber is moved to the specified location
+	 * 					A resource card is moved from the player being robbed's hand to the robbing player's hand
+	 * 					If applicable, largest army is awarded to the robbing player
+	 * 					No other Dev cards can be played this turn
+	 */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
