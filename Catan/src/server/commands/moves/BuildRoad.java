290fb0a2f05d138e3c1c50f19870c889af043553
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 22:59:21 -0700

    Build Road Javadocs Added

--- Catan/src/server/commands/moves/BuildRoad.java
+++ Catan/src/server/commands/moves/BuildRoad.java
@@ -10,6 +10,19 @@ public class BuildRoad extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+
+	
+	/**
+	 * Preconditions: The road location is open
+	 * 					The road is connected to another road owned by the player
+	 * 					The build location is not on water
+	 * 					The player has the required resources (1 wood, 1 brick, 1 road)
+	 * 					If it's settup round the location must be adjacent to a settlement owned by the player
+	 * 					with no adjacent road
+	 * Postconditions: The resources required to build a road are lost
+	 * 					The road is placed in the specified locatoin
+	 * 					If applicable, longest road is given to the player.
+	 */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
