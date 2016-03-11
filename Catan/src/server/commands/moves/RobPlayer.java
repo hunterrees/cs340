4ffabc0c88ab9d6e14ff9cb0e33e5be2ab09c304
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-10 21:58:15 -0700

    Added Javadocs to Rob Player

--- Catan/src/server/commands/moves/RobPlayer.java
+++ Catan/src/server/commands/moves/RobPlayer.java
@@ -10,6 +10,13 @@ public class RobPlayer extends Command {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Preconditions: The robber is being moved to a different location
+	 * 					The location the robber being moved to is legal
+	 * 					If a player is being robbed, the player being robbed has resource cards
+	 * Postconditions: The robber is moved to the new location
+	 * 					A resource card is moved from the hand of the player being robbed to the hand of the robber.
+	 */
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
