7ce5cc355499c6d06595d01d699db47ee48eed04
hunterrees <hunterrees13@gmail.com>
2016-03-10 18:20:08 -0700

    Added a translator class

---
+++ Catan/src/server/ServerTranslator.java
@@ -0,0 +1,20 @@
+package server;
+
+import shared.model.GameModel;
+
+public class ServerTranslator {
+	
+	private GameModel model;
+	
+	public ServerTranslator(GameModel model){
+		this.model = model;
+	}
+	/**
+	 * Translates the game model into json and returns it as a string
+	 * @return
+	 */
+	public String translate(){
+		return null;
+	}
+
+}
