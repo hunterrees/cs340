ab9b8e080835e0493101c7d654f3d6be09546f85
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-11 11:27:37 -0700

    Added Javadoc to Command class

--- Catan/src/server/commands/Command.java
+++ Catan/src/server/commands/Command.java
@@ -4,7 +4,13 @@ import shared.model.GameModel;
 
 public abstract class Command {
 	
+	/**
+	 * Contains the current model so that execute can make changes
+	 */
 	protected GameModel model;
+	/**
+	 * Contains the JSON string with the required parameters for execute
+	 */
 	protected String json;
 	
 	public Command(GameModel model, String json){
@@ -12,6 +18,10 @@ public abstract class Command {
 		this.json = json;
 	}
 
+	/**
+	 * Updates the model according to the command and parameters given
+	 * @return
+	 */
 	public abstract Object execute();
 
 }
