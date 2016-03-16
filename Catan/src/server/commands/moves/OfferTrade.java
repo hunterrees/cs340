573f70617795b01bab47b0e97c2aa79979494b00
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-15 22:35:30 -0600

    Added execute to offerTrade

--- Catan/src/server/commands/moves/OfferTrade.java
+++ Catan/src/server/commands/moves/OfferTrade.java
@@ -1,10 +1,22 @@
 package server.commands.moves;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+import client.translators.ModelTranslator;
+import client.translators.moves.ResourceList;
 import server.commands.Command;
+import shared.definitions.ResourceType;
 import shared.model.GameModel;
+import shared.model.trade.TradeOffer;
 
 public class OfferTrade extends Command{
 
+	private int playerIndex;
+	private int reciever;
+	private ResourceList offer;
+	
 	public OfferTrade(int gameID, String json) {
 		super(gameID, json);
 		// TODO Auto-generated constructor stub
@@ -13,10 +25,22 @@ public class OfferTrade extends Command{
 	 * Preconditions: The trader has the resources being offered
 	 * PostConditions: The tradeOffer class instance in the model is populated
 	 */
+	
+
 	@Override
 	public Object execute() {
 		// TODO Auto-generated method stub
-		return null;
+		ModelTranslator microTranslator = new ModelTranslator();
+		Gson gson = new Gson();
+		JsonObject root;
+		try{
+			root = gson.fromJson(json, JsonObject.class);
+		}catch(Exception e){
+			return model;
+		}
+		TradeOffer myTradeOffer = microTranslator.buildTradeOffer(root);
+		model.setTradeOffer(myTradeOffer);
+		return model;
 	}
 
 }
