72914e4327d01fdd545dc8532eebb2b045a74b3c
Brian <tiu.brian@byu.edu>
2016-03-16 13:07:58 -0600

    map updates - brian

--- Catan/src/server/commands/games/CreateGame.java
+++ Catan/src/server/commands/games/CreateGame.java
@@ -1,14 +1,176 @@
 package server.commands.games;
 
 import server.commands.Command;
+import shared.definitions.HexType;
+import shared.definitions.PortType;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
 import shared.model.GameModel;
+import shared.model.map.Port;
+import shared.model.map.TerrainHex;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 
 public class CreateGame extends Command{
 
+	shared.model.map.Map map;
 	public CreateGame(int gameID, String json) {
 		super(gameID, json);
 		// TODO Auto-generated constructor stub
 	}
+
+
+
+	public ArrayList<HexType> getHexTypeList() {
+		ArrayList<HexType> list = new ArrayList<>();
+
+		list.add(HexType.WOOD);
+		list.add(HexType.WOOD);
+		list.add(HexType.WOOD);
+
+		list.add(HexType.BRICK);
+		list.add(HexType.BRICK);
+
+		list.add(HexType.SHEEP);
+		list.add(HexType.SHEEP);
+		list.add(HexType.SHEEP);
+
+
+		list.add(HexType.WHEAT);
+		list.add(HexType.WHEAT);
+		list.add(HexType.WHEAT);
+
+
+		list.add(HexType.ORE);
+		list.add(HexType.ORE);
+
+		list.add(HexType.DESERT);
+
+
+		Collections.shuffle(list);
+		return list;
+	}
+
+	public void randomizeHexes() {
+		ArrayList<HexType> list = getHexTypeList();
+
+		int counter = 0;
+		for (java.util.Map.Entry<HexLocation, TerrainHex> entry : map.getHexes().entrySet()) {
+			if (entry.getValue().getType() != HexType.WATER) {
+				entry.getValue().setType(list.get(counter));
+				counter++;
+			}
+		}
+
+	}
+
+	public ArrayList<Integer> generateNumberPool() {
+		ArrayList<Integer> list = new ArrayList<>();
+
+		// Add the numbers in
+		list.add(2);
+
+		list.add(3);
+		list.add(3);
+
+		list.add(4);
+		list.add(4);
+
+		list.add(5);
+		list.add(5);
+
+		list.add(6);
+		list.add(6);
+
+		list.add(8);
+		list.add(8);
+
+		list.add(9);
+		list.add(9);
+
+		list.add(10);
+		list.add(10);
+
+		list.add(11);
+		list.add(11);
+
+		list.add(12);
+
+		// Shuffle the numbers
+		Collections.shuffle(list);
+
+		return list;
+	}
+
+
+	public void randomizeNumbers() {
+
+		ArrayList<Integer> numbers = generateNumberPool();
+		int counter = 0;
+
+
+		for(java.util.Map.Entry<HexLocation, TerrainHex> entry: map.getHexes().entrySet()){
+
+			if(entry.getValue().getType() != HexType.WATER &&
+					entry.getValue().getType() != HexType.DESERT) {
+
+				entry.getValue().setNumber(numbers.get(counter));
+				counter++;
+			}
+		}
+
+		if(counter != numbers.size()) {
+			System.out.println("Error! Didn't use correct number of numbers! Location: GetModel.generateRandomNumbers()");
+		}
+	}
+
+	public ArrayList<PortType> generatePortList() {
+		ArrayList<PortType> list = new ArrayList<>();
+
+		list.add(PortType.THREE);
+		list.add(PortType.THREE);
+		list.add(PortType.THREE);
+
+		list.add(PortType.WOOD);
+		list.add(PortType.BRICK);
+		list.add(PortType.SHEEP);
+		list.add(PortType.WHEAT);
+		list.add(PortType.ORE);
+
+		Collections.shuffle(list);
+
+		return list;
+
+	}
+
+
+	public void randomizePorts() {
+		ArrayList<PortType> list = generatePortList();
+
+		for(Map.Entry<VertexLocation, Port> entry : map.getPorts().entrySet()) {
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	/**
 	 * Preconditions: name is not null and randomTiles, randomNumbers, and randomPorts contain valid boolean values.
--- Catan/src/server/commands/moves/RollNumber.java
+++ Catan/src/server/commands/moves/RollNumber.java
@@ -1,14 +1,76 @@
 package server.commands.moves;
 
+import client.gameManager.GameManager;
 import server.commands.Command;
+import shared.definitions.HexType;
+import shared.definitions.PieceType;
+import shared.definitions.ResourceType;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
 import shared.model.GameModel;
+import shared.model.map.Map;
+import shared.model.map.TerrainHex;
+import shared.model.map.Vertex;
+import shared.model.player.Player;
 
 public class RollNumber extends Command{
+
+	Map map;
 
 	public RollNumber(int gameID, String json) {
 		super(gameID, json);
 		// TODO Auto-generated constructor stub
 	}
+
+
+
+	public void givePlayerResources(int roll) {
+		for(java.util.Map.Entry<HexLocation, TerrainHex> entry : map.getHexes().entrySet()) {
+			if(entry.getValue().getNumber() == roll && !map.getRobberLocation().equals(entry.getKey())) {
+				// Check every edge
+				for(java.util.Map.Entry<VertexLocation, Vertex> entryTwo : entry.getValue().getVerticies().entrySet()) {
+					if(entryTwo.getValue().getPiece() != null) {
+						addResource(entryTwo.getValue().getPiece().getPieceType(), entry.getValue().getType(), entryTwo.getValue().getPiece().getPlayerID());
+					}
+				}
+
+			}
+		}
+	}
+
+	public void addResource(PieceType pt, HexType ht, int playerIndex) {
+		Player p = model.getPlayers().get(playerIndex);
+		int amount = 0;
+
+		if(pt == PieceType.SETTLEMENT) {
+			amount = 1;
+		} else if(pt == PieceType.CITY) {
+			amount = 2;
+		} else {
+			System.out.println("Error! PieceType doesn't exist!")
+		}
+
+
+		switch(ht) {
+			case WOOD: p.getPlayerHand().addResources(amount, ResourceType.WOOD); break;
+			case BRICK: p.getPlayerHand().addResources(amount, ResourceType.BRICK); break;
+			case SHEEP: p.getPlayerHand().addResources(amount, ResourceType.SHEEP); break;
+			case WHEAT: p.getPlayerHand().addResources(amount, ResourceType.WHEAT); break;
+			case ORE: p.getPlayerHand().addResources(amount, ResourceType.ORE); break;
+			default: System.out.println("Error! HexType doesn't exist!");
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
 
 	/**
 	 * Preconditions: It's the players turn
