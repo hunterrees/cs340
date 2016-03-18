d4e919d195bf095a972bfc58ef430e10b401c182
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-18 09:06:08 -0600

    Added execute functionality to finishTurn

--- Catan/src/server/commands/moves/FinishTurn.java
+++ Catan/src/server/commands/moves/FinishTurn.java
@@ -1,5 +1,10 @@
 package server.commands.moves;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+import client.server.ServerException;
 import server.commands.Command;
 import shared.model.GameModel;
 
@@ -14,11 +19,49 @@ public class FinishTurn extends Command {
 	 * Preconditions: The game is in the correct state
 	 * PostConditions: Cards in the newDevCards hand are transferred to the oldDevCards hand.
 	 * 					The turn counter is incremented.
+	 * @throws ServerException 
 	 */
+	
+	private void incrementTurn(int playerIndex)
+	{
+		int nextTurn = 0;
+		if (playerIndex == 3)
+		{
+			nextTurn = 0;
+		}
+		else 
+		{
+			nextTurn = playerIndex + 1;
+		}
+		model.getTracker().setCurrentTurnPlayerID(nextTurn);
+	}
+	
+	private void newDevCardsToOld(int playerIndex)
+	{
+		model.getPlayers().get(playerIndex).getPlayerHand().newDevCardsToOld();
+	}
+	
 	@Override
-	public Object execute() {
+	public Object execute() throws ServerException {
 		// TODO Auto-generated method stub
-		return null;
+		int playerIndex;
+		Gson gson = new Gson();
+		JsonObject root;
+		try{
+			root = gson.fromJson(json, JsonObject.class);
+		}catch(Exception e){
+			return null;
+		}
+		playerIndex = root.getAsJsonPrimitive("playerIndex").getAsInt();
+		playingState();
+		myTurn(playerIndex);
+		
+		//increment player turn
+		incrementTurn(playerIndex);
+		newDevCardsToOld(playerIndex);
+		
+		return model;
+		
 	}
 
 }
