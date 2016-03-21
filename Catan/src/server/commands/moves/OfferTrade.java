63ca267d02e2a66424503b5e2a96a863fe75c1e7
ztnelson01 <zach.t.nelson01@gmail.com>
2016-03-21 07:51:23 -0600

    Added offerTrade execute

--- Catan/src/server/commands/moves/OfferTrade.java
+++ Catan/src/server/commands/moves/OfferTrade.java
@@ -1,5 +1,7 @@
 package server.commands.moves;
 
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
@@ -7,7 +9,9 @@ import com.google.gson.JsonPrimitive;
 import client.server.ServerException;
 import client.translators.ModelTranslator;
 import client.translators.moves.ResourceList;
+import server.ServerTranslator;
 import server.commands.Command;
+import shared.ResourceCard;
 import shared.definitions.ResourceType;
 import shared.model.GameModel;
 import shared.model.trade.TradeOffer;
@@ -17,6 +21,8 @@ public class OfferTrade extends Command{
 	private int playerIndex;
 	private int reciever;
 	private ResourceList offer;
+	
+	private TradeOffer completedOffer;
 	
 	public OfferTrade(int gameID, String json) {
 		super(gameID, json);
@@ -28,24 +34,69 @@ public class OfferTrade extends Command{
 	 * @throws ServerException 
 	 */
 	
-
-	@Override
-	public Object execute() throws ServerException {
-		// TODO Auto-generated method stub
-		ModelTranslator microTranslator = new ModelTranslator();
+	private void addTradeResources(ArrayList<ResourceType> offer, ArrayList<ResourceType> receive, ResourceType type, int amount){
+		if(amount > 0){
+			for(int i = 0; i < amount; i++){
+				receive.add(type);
+			}
+		}else if(amount < 0){
+			amount = -1 * amount;
+			for(int i = 0; i < amount; i++){
+				offer.add(type);
+			}
+		}
+	}
+	
+	private void translate()
+	{
 		Gson gson = new Gson();
 		JsonObject root;
 		try{
 			root = gson.fromJson(json, JsonObject.class);
 		}catch(Exception e){
-			return model;
+			return;
 		}
-		int playerIndex = root.getAsJsonPrimitive("playerIndex").getAsInt();
+	
+		playerIndex = root.getAsJsonPrimitive("playerIndex").getAsInt();
+		reciever = root.getAsJsonPrimitive("receiver").getAsInt();
+		
+		JsonObject offerJson = root.getAsJsonObject("offer");
+		JsonPrimitive brickJson = offerJson.getAsJsonPrimitive("brick");
+		int brick = brickJson.getAsInt();
+		
+		JsonPrimitive woodJson = offerJson.getAsJsonPrimitive("wood");
+		int wood = woodJson.getAsInt();
+		
+		JsonPrimitive sheepJson = offerJson.getAsJsonPrimitive("sheep");
+		int sheep = sheepJson.getAsInt();
+		
+		JsonPrimitive wheatJson = offerJson.getAsJsonPrimitive("wheat");
+		int wheat = wheatJson.getAsInt();
+		
+		JsonPrimitive oreJson = offerJson.getAsJsonPrimitive("ore");
+		int ore = oreJson.getAsInt();
+		
+		ArrayList<ResourceType> resourcesOffered = new ArrayList<ResourceType>();
+		ArrayList<ResourceType> resourcesDesired = new ArrayList<ResourceType>();
+		addTradeResources(resourcesOffered, resourcesDesired, ResourceType.BRICK, brick);
+		addTradeResources(resourcesOffered, resourcesDesired, ResourceType.WOOD, wood);
+		addTradeResources(resourcesOffered, resourcesDesired, ResourceType.SHEEP, sheep);
+		addTradeResources(resourcesOffered, resourcesDesired, ResourceType.WHEAT, wheat);
+		addTradeResources(resourcesOffered, resourcesDesired, ResourceType.ORE, ore);
+		
+		completedOffer = new TradeOffer(playerIndex, reciever, resourcesOffered, resourcesDesired);
+	}
+	
+	@Override
+	public Object execute() throws ServerException {
+		// TODO Auto-generated method stub
 		playingState();
 		myTurn(playerIndex);
-		TradeOffer myTradeOffer = microTranslator.buildTradeOffer(root);
-		model.setTradeOffer(myTradeOffer);
-		return model;
+		translate();
+		model.setTradeOffer(completedOffer);
+		
+		ServerTranslator temp = new ServerTranslator(model);
+		return temp.translate();
 	}
 
 }
