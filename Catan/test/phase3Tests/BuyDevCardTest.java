2b2cb29bda38cb034d8e63657c9da577915d3b49
Brian <tiu.brian@byu.edu>
2016-03-22 22:32:45 -0600

    map updates - brian

---
+++ Catan/test/phase3Tests/BuyDevCardTest.java
@@ -0,0 +1,34 @@
+package phase3Tests;
+
+import client.server.ServerException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import server.commands.moves.BuildSettlement;
+import shared.definitions.PieceType;
+import shared.locations.HexLocation;
+import shared.locations.VertexDirection;
+import shared.locations.VertexLocation;
+import shared.model.GameModel;
+import shared.model.map.Map;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.* ;
+
+
+/**
+ * Created by briantiu on 3/21/16.
+ */
+public class BuyDevCardTest {
+    @Test
+    public void test() {
+
+        String
+
+    }
+
+
+}
