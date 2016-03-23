47f0a777c2de139c1dc0e42bd3fe08473e94eb71
briantiu <tiu.brian@byu.edu>
2016-03-22 20:08:01 -0600

    asdf

--- Catan/src/server/commands/moves/BuildRoad.java
+++ Catan/src/server/commands/moves/BuildRoad.java
@@ -1,5 +1,6 @@
 package server.commands.moves;
 
+import client.server.ServerException;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
@@ -81,7 +82,7 @@ public class BuildRoad extends Command {
 	 * 					If applicable, longest road is given to the player.
 	 */
 	@Override
-	public Object execute() {
+	public Object execute() throws ServerException {
 		// TODO Auto-generated method stub
 		Player p = model.getPlayers().get(playerIndex);
 
