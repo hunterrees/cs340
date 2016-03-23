d7a0344cdb300b816eef429aa694527090e0bca0
hunterrees <hunterrees13@gmail.com>
2016-03-23 11:54:06 -0600

    Junits for my part done

---
+++ Catan/test/phase3Tests/GetModelTest.java
@@ -0,0 +1,35 @@
+package phase3Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import client.server.ServerException;
+import client.translators.ModelTranslator;
+import server.ServerTranslator;
+import server.commands.game.GetModel;
+import shared.model.GameModel;
+
+public class GetModelTest {
+
+	@Test
+	public void test() {
+		GetModel getModel = new GetModel(0, "");
+		String json = "";
+		try {
+			json = (String) getModel.execute();
+		} catch (ServerException e) {
+			fail();
+		}
+		try{
+			ModelTranslator translator = new ModelTranslator();
+			GameModel model = translator.getModelfromJSON(json);
+			ServerTranslator serverTranslate = new ServerTranslator(model);
+			String newJson = serverTranslate.translate();
+			assertTrue(newJson.equals(json));
+		}catch(Exception e){
+			fail();
+		}
+	}
+
+}
---
+++ Catan/test/phase3Tests/RegisterTest.java
@@ -0,0 +1,46 @@
+package phase3Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import client.translators.user.UserTranslator;
+import server.commands.user.Register;
+
+public class RegisterTest {
+
+	@Test
+	public void test() {
+		UserTranslator user = new UserTranslator("Sam", "samuel");
+		Register register = new Register(-1, user.translate());
+		try{
+			register.execute();
+			fail();
+		}catch(Exception e){
+		}
+		user.setUsername("");
+		register = new Register(-1, user.translate());
+		try{
+			register.execute();
+			fail();
+		}catch(Exception e){
+		}
+		user.setUsername("Sam");
+		user.setPassword("sam");
+		register = new Register(-1, user.translate());
+		try{
+			register.execute();
+			fail();
+		}catch(Exception e){
+		}
+		user.setUsername("Hunter");
+		user.setPassword("hunter");
+		register = new Register(-1, user.translate());
+		try{
+			register.execute();
+		}catch(Exception e){
+			fail();
+		}
+	}
+
+}
