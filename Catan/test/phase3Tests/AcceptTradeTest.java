39ff2ef4c0ae2dd08ab4eed68f962cf0fa397a72
Brian <tiu.brian@byu.edu>
2016-03-23 20:26:39 -0600

    map updates - brian

--- Catan/test/phase3Tests/AcceptTradeTest.java
+++ Catan/test/phase3Tests/AcceptTradeTest.java
@@ -43,7 +43,7 @@ public class AcceptTradeTest {
         ArrayList<ResourceType> giving = new ArrayList<>();
         ArrayList<ResourceType> receiving = new ArrayList<>();
 
-        TradeOffer trade = new TradeOffer()
+        TradeOffer trade = new TradeOffer(0, 1, giving, receiving);
 
         for(int i = 0; i < 1; i++) giving.add(ResourceType.BRICK);
         for(int i = 0; i < 2; i++) giving.add(ResourceType.ORE);
@@ -62,12 +62,33 @@ public class AcceptTradeTest {
 
         try {
 
+            int beforeBrick1 = p1.getPlayerHand().numResourceOfType(ResourceType.BRICK);
+            int beforeOre1 = p1.getPlayerHand().numResourceOfType(ResourceType.ORE);;
+            int beforeSheep1 = p1.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
+            int beforeWheat1 =  p1.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
+            int beforeWood1 = p1.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
+            int beforeBrick2 = p1.getPlayerHand().numResourceOfType(ResourceType.BRICK);
+            int beforeOre2= p1.getPlayerHand().numResourceOfType(ResourceType.ORE);;
+            int beforeSheep2= p1.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
+            int beforeWheat2= p1.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
+            int beforeWood2= p1.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
 
             command.getModel().getTracker().setGameStatus(GameState.playing);
             command.execute();
 
+            int afterBrick1= p2.getPlayerHand().numResourceOfType(ResourceType.BRICK);;
+            int afterOre1= p2.getPlayerHand().numResourceOfType(ResourceType.ORE);;
+            int afterSheep1= p2.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
+            int afterWheat1= p2.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
+            int afterWood1= p2.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
+
+            int afterBrick2= p2.getPlayerHand().numResourceOfType(ResourceType.BRICK);;
+            int afterOre2= p2.getPlayerHand().numResourceOfType(ResourceType.ORE);;
+            int afterSheep2= p2.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
+            int afterWheat2= p2.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
+            int afterWood2= p2.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
 
 
