f8902d51c4a67c04f8a7e70ee730f92e2926aa25
hunterrees <hunterrees13@gmail.com>
2016-03-23 20:30:26 -0600

    Roll Number j-unit

--- Catan/test/phase1tests/Phase1Tests.java
+++ Catan/test/phase1tests/Phase1Tests.java
@@ -29,7 +29,11 @@ public class Phase1Tests{
 				"canDoTests.CanUseMonument", "canDoTests.CanUseRoadBuilder", "canDoTests.CanUseSoldier", "canDoTests.CanUseYearOfPlenty",
 				"serverTests.ServerProxyTests", "serverTests.ServerPollerTests", "serverTests.InitializeModel", "phase3Tests.LoginTest",
 				"phase3Tests.RegisterTest", "phase3Tests.JoinGameTest", "phase3Tests.CreateGameTest", "phase3Tests.ListGamesTest", 
-				"phase3Tests.ListAITest", "phase3Tests.AddAITest", "phase3Tests.GetModelTest", "phase3Tests.SendChatTest"
+				"phase3Tests.ListAITest", "phase3Tests.AddAITest", "phase3Tests.GetModelTest", "phase3Tests.SendChatTest", 
+				"phase3Tests.AcceptTradeTest", "phase3Tests.BuildCityTest", "phase3Tests.BuildRoadTest", "phase3Tests.BuildSettlementTest",
+				"phase3Tests.BuyDevCardTest", "phase3Tests.DiscardTest", "phase3Tests.FinishTurnTest", "phase3Tests.MaritimeTradeTest",
+				"phase3Tests.MonopolyTest", "phase3Tests.MonumentTest", "phase3Tests.OfferTradeTest", "phase3Tests.RoadBuildTest",
+				"phase3Tests.RobPlayerTest", "phase3Tests.SoldierTest", "phase3Tests.YearOfPlentyTest"
 		}; 
 
 		org.junit.runner.JUnitCore.main(testClasses);
