b5c879e3d627e2f27277b55a68e611de8e01c149
Brian <tiu.brian@byu.edu>
2016-03-23 20:50:31 -0600

    map updates - brian

--- Catan/test/phase3Tests/AcceptTradeTest.java
+++ Catan/test/phase3Tests/AcceptTradeTest.java
@@ -50,15 +50,15 @@ public class AcceptTradeTest {
 
         for(int i = 0; i < 1; i++) giving.add(ResourceType.BRICK);
         for(int i = 0; i < 2; i++) giving.add(ResourceType.ORE);
-        for(int i = 0; i < 3; i++) giving.add(ResourceType.WOOD);
+        for(int i = 0; i < 3; i++) giving.add(ResourceType.SHEEP);
         for(int i = 0; i < 4; i++) giving.add(ResourceType.WHEAT);
-        for(int i = 0; i < 5; i++) giving.add(ResourceType.SHEEP);
+        for(int i = 0; i < 5; i++) giving.add(ResourceType.WOOD);
 
         for(int i = 0; i < 5; i++) receiving.add(ResourceType.BRICK);
         for(int i = 0; i < 4; i++) receiving.add(ResourceType.ORE);
-        for(int i = 0; i < 3; i++) receiving.add(ResourceType.WOOD);
+        for(int i = 0; i < 3; i++) receiving.add(ResourceType.SHEEP);
         for(int i = 0; i < 2; i++) receiving.add(ResourceType.WHEAT);
-        for(int i = 0; i < 1; i++) receiving.add(ResourceType.SHEEP);
+        for(int i = 0; i < 1; i++) receiving.add(ResourceType.WOOD);
 
         TradeOffer trade = new TradeOffer(0, 1, giving, receiving);
         command.getModel().setTradeOffer(trade);
@@ -73,21 +73,21 @@ public class AcceptTradeTest {
             int beforeWheat1 =  p1.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
             int beforeWood1 = p1.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
-            int beforeBrick2 = p1.getPlayerHand().numResourceOfType(ResourceType.BRICK);
-            int beforeOre2= p1.getPlayerHand().numResourceOfType(ResourceType.ORE);;
-            int beforeSheep2= p1.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
-            int beforeWheat2= p1.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
-            int beforeWood2= p1.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
+            int beforeBrick2 = p2.getPlayerHand().numResourceOfType(ResourceType.BRICK);
+            int beforeOre2= p2.getPlayerHand().numResourceOfType(ResourceType.ORE);;
+            int beforeSheep2= p2.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
+            int beforeWheat2= p2.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
+            int beforeWood2= p2.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
 
             command.getModel().getTracker().setGameStatus(GameState.playing);
             command.execute();
 
-            int afterBrick1= p2.getPlayerHand().numResourceOfType(ResourceType.BRICK);;
-            int afterOre1= p2.getPlayerHand().numResourceOfType(ResourceType.ORE);;
-            int afterSheep1= p2.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
-            int afterWheat1= p2.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
-            int afterWood1= p2.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
+            int afterBrick1= p1.getPlayerHand().numResourceOfType(ResourceType.BRICK);;
+            int afterOre1= p1.getPlayerHand().numResourceOfType(ResourceType.ORE);;
+            int afterSheep1= p1.getPlayerHand().numResourceOfType(ResourceType.SHEEP);;
+            int afterWheat1= p1.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
+            int afterWood1= p1.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
             int afterBrick2= p2.getPlayerHand().numResourceOfType(ResourceType.BRICK);;
             int afterOre2= p2.getPlayerHand().numResourceOfType(ResourceType.ORE);;
@@ -95,17 +95,17 @@ public class AcceptTradeTest {
             int afterWheat2= p2.getPlayerHand().numResourceOfType(ResourceType.WHEAT);;
             int afterWood2= p2.getPlayerHand().numResourceOfType(ResourceType.WOOD);;
 
-            assertTrue(beforeBrick1 == afterBrick1);
-            assertTrue(beforeOre1 == afterOre1);
-            assertTrue(beforeSheep1 == afterSheep1);
-            assertTrue(beforeWheat1 == afterWheat1);
-            assertTrue(beforeWood1 == afterWood1);
+            assertTrue(beforeBrick1-5+1 == afterBrick1);
+            assertTrue(beforeOre1-4+2 == afterOre1);
+            assertTrue(beforeSheep1-3+3 == afterSheep1);
+            assertTrue(beforeWheat1-2+4 == afterWheat1);
+            assertTrue(beforeWood1-1+5 == afterWood1);
 
-            assertTrue(beforeBrick2 == afterBrick2);
-            assertTrue(beforeOre2 == afterOre2);
-            assertTrue(beforeSheep2 == afterSheep2);
-            assertTrue(beforeWheat2 == afterWheat2);
-            assertTrue(beforeWood2 == afterWood2);
+            assertTrue(beforeBrick2-1+5 == afterBrick2);
+            assertTrue(beforeOre2-2+4 == afterOre2);
+            assertTrue(beforeSheep2-3+3 == afterSheep2);
+            assertTrue(beforeWheat2-4+2 == afterWheat2);
+            assertTrue(beforeWood2-5+1 == afterWood2);
 
 
 
