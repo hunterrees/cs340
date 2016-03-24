e3ca4ed11f6b4c38f1ea1f7b1781be6047579c0b
Brian <tiu.brian@byu.edu>
2016-03-23 20:19:04 -0600

    map updates - brian

--- Catan/test/phase3Tests/AcceptTradeTest.java
+++ Catan/test/phase3Tests/AcceptTradeTest.java
@@ -4,6 +4,9 @@ import client.server.ServerException;
 import org.junit.Test;
 import server.commands.moves.AcceptTrade;
 import shared.definitions.GameState;
+import shared.definitions.ResourceType;
+import shared.model.player.Player;
+import shared.model.trade.TradeOffer;
 
 import static org.junit.Assert.* ;
 
@@ -18,7 +21,22 @@ public class AcceptTradeTest {
 
         AcceptTrade command = new AcceptTrade(0, json);
 
+        Player p1 = command.getModel().getPlayers().get(0);
+        p1.getPlayerHand().addResources(10, ResourceType.BRICK);
+        p1.getPlayerHand().addResources(10, ResourceType.ORE);
+        p1.getPlayerHand().addResources(10, ResourceType.WOOD);
+        p1.getPlayerHand().addResources(10, ResourceType.WHEAT);
+        p1.getPlayerHand().addResources(10, ResourceType.SHEEP);
 
+        Player p2 = command.getModel().getPlayers().get(1);
+        p2.getPlayerHand().addResources(10, ResourceType.BRICK);
+        p2.getPlayerHand().addResources(10, ResourceType.ORE);
+        p2.getPlayerHand().addResources(10, ResourceType.WOOD);
+        p2.getPlayerHand().addResources(10, ResourceType.WHEAT);
+        p2.getPlayerHand().addResources(10, ResourceType.SHEEP);
+
+
+        TradeOffer trade = new TradeOffer(0, 1, null, null);
 
 
 
