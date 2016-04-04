8b5cf36804d24edeed55166f6541fd5b0aa80320
ztnelson01 <zach.t.nelson01@gmail.com>
2016-04-04 11:04:24 -0600

    Added UserDAO methods

--- Catan/src/persistance/dbJar/DBUserDAO.java
+++ Catan/src/persistance/dbJar/DBUserDAO.java
@@ -1,5 +1,22 @@
 package persistance.dbJar;
 
-public class DBUserDAO {
+import java.util.ArrayList;
+
+import persistance.interFaceJar.UserDAO;
+import server.User;
+
+public class DBUserDAO implements UserDAO {
+
+	@Override
+	public ArrayList<User> getUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User userToAdd) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Catan/src/persistance/interFaceJar/UserDAO.java
+++ Catan/src/persistance/interFaceJar/UserDAO.java
@@ -1,12 +1,19 @@
 package persistance.interFaceJar;
 
+import java.util.ArrayList;
+
 import server.User;
 
 public interface UserDAO {
 
 	/**
-	 * 
+	 * Return list of all users from persistance
 	 * @return
 	 */
-	public User getUser();
+	public ArrayList<User> getUsers();
+	
+	/**
+	 * Adds a single user to Persistance
+	 */
+	public void addUser(User userToAdd);
 }
